43304fad17af40ddbc9e568dd268aaead01a9a6e
aayushi.m.jain <aayushi0716@gmail.com>
2021-12-17 15:04:30 +0530

    addition of webflux

---
+++ src/main/java/com/customer/exception/CustomerException.java
@@ -0,0 +1,27 @@
+package com.customer.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@ControllerAdvice
+public class CustomerException {
+
+    @ExceptionHandler(value = CustomerNotFoundException.class)
+    public ResponseEntity<?> customerNotFoundException(CustomerNotFoundException exception, WebRequest request){
+        ErrorDetails errorDetails =
+                new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> globalExceptionHandling(Exception exception, WebRequest request){
+        ErrorDetails errorDetails =
+                new ErrorDetails(new Date(), exception.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ src/main/java/com/customer/exception/CustomerNotFoundException.java
@@ -0,0 +1,9 @@
+package com.customer.exception;
+
+public class CustomerNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+    public CustomerNotFoundException(String message) {
+        super(message);}
+}
+
+//spring data
